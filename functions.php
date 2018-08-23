efc5b260d84637a8589efedbf1a94fdbc25ada35
CP Müller <support@webdesigndenhaag.net>
2018-08-23 14:12:36 +0200

    setup functions

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * theme main functions file
+ */
+ 
+// require get_template_directory() . '/customizer.php'; // customizer functions
+// require get_template_directory() . '/data.php'; // data functions
+
+// register options
+function theme_post_thumbnails() {
+    add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'theme_post_thumbnails' );
+
+// insert style sheet short function
+function wp_main_theme_stylesheet(){
+    $stylesheet = dirname( __file__ ).'/style.css';
+    echo '<link rel="stylesheet" id="wp-theme-main-style"  href="'.$stylesheet.'" type="text/css" media="all" />';
+}
+?>
